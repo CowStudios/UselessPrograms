a44f3b9a2589ea879745dd05a2985efac3f256d4
Piotr Bartela <portfel666@gmail.com>
2015-10-13 21:53:54 +0200

    Kawalki strumienia

---
+++ src/main/CosZStringa.java
@@ -0,0 +1,20 @@
+package main;
+
+public class CosZStringa {
+
+	public static void main(String[] args) {
+		// Sting wejsciowy.
+		String koza = "Cos Kot, nie wiem.";
+		
+		// To cos to wyciagania.
+		String output = koza.substring(4, 5);
+		
+		// Tylko jednen znak. 
+		char outputchat = koza.charAt(4);
+		
+		
+		System.out.println(output);
+		System.out.println(outputchat);
+	}
+
+}
